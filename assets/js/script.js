8239676f9a135ef72fc068156bf8bf2d39588d3a
Ankur <ankurshahi@Ankurs-iMac.phub.net.cable.rogers.com>
2021-05-30 18:11:49 -0400

    city name passed from the HTLM page to JavaScript for fetching user input

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,6 @@
 var apiKey="b9abceb7e557e8af7a7048ba9cc67d90";
+var userRequestEl=document.querySelector("#user-request");
+var cityNameEl=document.querySelector("#city-name");
 var getCityWeather=function(city) {
     // format the openweathermap api url
     var apiUrl = "http://api.openweathermap.org/data/2.5/forecast?q="+city+"&appid="+apiKey;
@@ -10,4 +12,17 @@ var getCityWeather=function(city) {
         });
     });
 };
-getCityWeather("toronto");
+
+var formSubmitHandler = function(event) {
+    event.preventDefault();
+    var city = cityNameEl.value.trim();
+
+    if (city) {
+        getCityWeather(city);
+        cityNameEl.value="";
+    } else {
+        alert("Please enter a correct city name");
+    }
+};
+userRequestEl.addEventListener("submit",formSubmitHandler);
+// getCityWeather("toronto");
