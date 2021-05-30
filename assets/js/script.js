2ed75f130cc3d261a2d22acc2b85aef27ba2259a
Ankur <ankurshahi@Ankurs-iMac.phub.net.cable.rogers.com>
2021-05-30 17:36:31 -0400

    dynamic city request logic added

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,13 @@
 var apiKey="b9abceb7e557e8af7a7048ba9cc67d90";
 var getCityWeather=function(city) {
-    fetch("http://api.openweathermap.org/data/2.5/forecast?q="+city+"&appid="+apiKey);
+    // format the openweathermap api url
+    var apiUrl = "http://api.openweathermap.org/data/2.5/forecast?q="+city+"&appid="+apiKey;
+    
+    // make a request to the url
+    fetch(apiUrl).then(function(response){
+        response.json().then(function(data){
+            console.log(data);
+        });
+    });
 };
 getCityWeather("toronto");
